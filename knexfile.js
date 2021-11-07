5c73e52a30998c6afa4cb547ddd9bbebe6074314
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-11-07 12:41:28 -0800

    setup knex file, had to use npx knex to initialize. Right now it has the client as sqlite3. there are comments all throughout the file indication what each line does

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    //will be changing this to postgres
+    client: "sqlite3",
+    //this is for a specific error that happens in sqlite
+    useNullAsDefault: true,
+    connection: {
+      //this is for the location and name of the database
+      filename: "./dev.sqlite3",
+    },
+    migrations: {
+      // location of migrations for the database
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
