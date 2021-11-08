457077b1e1576230aacbb0d967bf781ba4827d64
Katiuska Alicea <katiuskaalicea@gmail.com>
2021-11-07 17:04:25 -0800

    imported db from user-model to urse-router and created get request that returns an array of users

--- api/user-router.js
+++ api/user-router.js
@@ -1,4 +1,17 @@
 // here we are both importing express and its Router method
 const router = require("express").Router();
+const db = require("./user-model");
+
+router.get("/", (req, res) => {
+  db.find()
+    .then((user) => {
+      const { username, id, role } = user;
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ message: "could not find users" });
+    });
+});
 
 module.exports = router;
